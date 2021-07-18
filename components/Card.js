3c0a63332b5357bb7e80b4f1e9108e8eaf536273
Glenn Meyer <grossmeyer@gmail.com>
2021-07-18 11:28:51 -0400

    Reformatted card section layout

--- components/Card.js
+++ components/Card.js
@@ -1,19 +1,19 @@
 import Image from 'next/image'
 
-export default function Card({ title, link, image = '/images/logo-white.png', altText = 'Alt Text', shortText }) {
+export default function Card({ title, link, image = '/images/logo-white.png', altText = 'Alt Text', previewText }) {
   return (
-    <div className='max-w-xs justify-self-center m-4 hover:border-2 hover:border-synthpurple-100'>
-      <a href={link} className='flex flex-col items-center rounded-2xl shadow-lg bg-synthblue-300 text-synthpurple-1000 hover:bg-synthblue-500 pt-3 px-2 no-underline'>
-        <Image
-          src={image}
-          alt={altText}
-          width={200}
-          height={100}
-          className='rounded-lg'
-        />
-        <h3 className='text-2xl pt-1 font-header'>{title} &rarr;</h3>
-        <p className='p-2 font-typeface text-center'>{shortText}</p>
-      </a>
-    </div>
+    <a href={link} className='flex flex-row space-x-4 rounded-2xl bg-synthpink-100 hover:bg-synthpink-300 p-4 m-4 no-underline text-synthpurple-800 hover:text-synthpurple-1000'>
+      <Image
+        src={image}
+        alt={altText}
+        width={150}
+        height={75}
+        className='rounded-lg flex-grow-0 flex-shrink-0'
+      />
+      <div>
+        <h3 className='text-3xl font-header flex-grow'>{title}</h3>
+        <p className='pt-1 font-typeface flex-shrink'>{previewText}</p>
+      </div>
+    </a>
   )
 }
--- components/CardSection.js
+++ components/CardSection.js
@@ -2,15 +2,15 @@ import Card from './Card'
 
 export default function CardSection({ posts }) {
   return (
-    <section className='pb-2 md:pb-4 mb-auto flex-grow'>
-      <div className='grid grid-cols-1 gap-6 md:gap-1'>
-        {posts.map(({ title, slug }, index) => {
+    <section className='pb-2 md:pb-4 mb-auto'>
+      <div className='flex flex-col'>
+        {posts.map(({ title, slug, previewText }, index) => {
           return (
             <Card
               key={index}
               title={title}
               link={`posts/${slug.current}`}
-              shortText={title}
+              previewText={title}
             >
             </Card>
           )
@@ -21,7 +21,7 @@ export default function CardSection({ posts }) {
           link='https://nextjs.org/docs'
           image='/images/reference-docs.jpg'
           altText='Picture of dusty old library books'
-          shortText='Find in-depth information about Next.js features and API.'
+          previewText='Find in-depth information about Next.js features and API.'
         />
 
         <Card
@@ -29,7 +29,7 @@ export default function CardSection({ posts }) {
           link='https://nextjs.org/learn'
           image='/images/learning-sign.jpg'
           altText='Picture of sign reading "Love To Learn"'
-          shortText='Learn about Next.js in an interactive course with quizzes!'
+          previewText='Learn about Next.js in an interactive course with quizzes!'
         />
 
         <Card
@@ -37,7 +37,7 @@ export default function CardSection({ posts }) {
           link='https://github.com/vercel/next.js/tree/master/examples'
           image='/images/projects-sign.jpg'
           altText='Picture of sign reading "Projects"'
-          shortText='Discover and deploy boilerplate example Next.js projects.'
+          previewText='Discover and deploy boilerplate example Next.js projects.'
         />
 
         <Card
@@ -45,7 +45,7 @@ export default function CardSection({ posts }) {
           link='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
           image='/images/deploy-building.jpg'
           altText='Picture of futuristic grid-like building'
-          shortText='Instantly deploy your Next.js site to a public URL with Vercel.'
+          previewText='Instantly deploy your Next.js site to a public URL with Vercel.'
         />
       </div>
     </section>
